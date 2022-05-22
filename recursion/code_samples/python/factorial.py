79a17ae4446a08a1812cf59a385849a42b52455c
Reuben deVries <reuben.devries@bell.ca>
2022-05-22 12:34:27 -0700

    added assert to factorial.py

--- recursion/code_samples/python/factorial.py
+++ recursion/code_samples/python/factorial.py
@@ -1,9 +1,11 @@
 def factorial_number(n):
     '''An example of a recursive factorial function written in python.'''
-    if n < 2:
+    assert n >= 0 and int(n) == n, "Factorial Sums must be calaculated from a positive integer"
+    if n <= 1:
         return 1
     return n * factorial_number(n-1)
+    
 
 if __name__ == "__main__":
-    x = factorial_number(10)
+    x = factorial_number(-10)
     print(x)
